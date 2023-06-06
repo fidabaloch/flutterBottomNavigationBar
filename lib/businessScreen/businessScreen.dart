fbb54e281ffa715926c97c213a0e32c5e2cb6a97
fidabaloch <fida_baloch@hotmail.com>
2023-06-06 13:23:28 +0500

    Gesture Detector

--- lib/businessScreen/businessScreen.dart
+++ lib/businessScreen/businessScreen.dart
@@ -4,7 +4,21 @@ class BusinessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Business Screen'),
-    );
+        child: GestureDetector(
+      onTap: () {
+        print('Single Tap');
+      },
+      onDoubleTap: () {
+        print('Double Tap');
+      },
+      onLongPress: () {
+        print('Long Press');
+      },
+      child: Container(
+        height: 100,
+        width: 100,
+        color: Colors.red,
+      ),
+    ));
   }
 }
