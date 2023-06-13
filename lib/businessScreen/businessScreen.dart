78ee18aabc34d9a05c865796f4f81244d9a630f2
fidabaloch <fida_baloch@hotmail.com>
2023-06-13 11:43:24 +0500

    List View

--- lib/businessScreen/businessScreen.dart
+++ lib/businessScreen/businessScreen.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 
-class BusinessScreen extends StatelessWidget {
+class BusinessScreen extends StatefulWidget {
   @override
+  State<BusinessScreen> createState() => _BusinessScreenState();
+}
+
+class _BusinessScreenState extends State<BusinessScreen> {
+  var containerColor = Colors.red;
+  @override
+  //By Pressing Set State the code will be rebuild from Build fucnction
   Widget build(BuildContext context) {
+    // int i = 5;
+    // String s = "new sting";
+    // var j = "4.5";
+    print("i am rebuilding");
     return Center(
         child: GestureDetector(
       onTap: () {
+        setState(() {
+          containerColor = Colors.blue;
+        });
         print('Single Tap');
       },
       onDoubleTap: () {
+        setState(() {
+          containerColor = Colors.green;
+        });
         print('Double Tap');
       },
       onLongPress: () {
+        setState(() {
+          containerColor = Colors.yellow;
+        });
         print('Long Press');
       },
       child: Container(
         height: 100,
         width: 100,
-        color: Colors.red,
+        color: containerColor,
       ),
     ));
   }
